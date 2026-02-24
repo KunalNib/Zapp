a07c42b51bff0a30b574635a87f239361d68d77a
Kunal Nibrad <kunalnibrad297@gmail.com>
2026-02-24 23:01:07 +0530

    email verification

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,8 @@
 import User from "../models/userModel.js";
+import jwt from "jsonwebtoken"
+import bcrypt from "bcryptjs";
+import { verifyEmail } from "../emailVerify/verifyEmail.js";
+
 export const register=async(req,res)=>{
     try{
         const {firstName,lastName,email,password}=req.body;
@@ -9,15 +13,67 @@ export const register=async(req,res)=>{
         if(user){
             return res.status(400).json({success:false,message:"User already exists"});
         }
+        const hashedPassword=await bcrypt.hash(password,10);//10->salt
         const newUser=await User.create({
             firstName,
             lastName,
             email,
-            password
+            password:hashedPassword
         });
+        const token=jwt.sign({id:newUser._id},process.env.SECRET_KEY,{expiresIn:"10m"}); //expires in 10 min
+        verifyEmail(token,email);
+        newUser.token=token;
         await newUser.save();
         return res.status(201).json({success:true,message:"User registered successfully",user:newUser});
     }catch(error){
         res.status(500).json({success:false,message:error.message});
+    }
+}
+
+export const verify=async(req,res)=>{
+    try{
+        const authHeader=req.headers.authorization
+        if(!authHeader || !authHeader.startsWith("Bearer ")){
+            res.status(400).json({
+                success:false,
+                message:"Authorization token is missing or invalid"
+            })
+        }
+        const token=authHeader.split(" ")[1] // [Bearer ,token----]
+        let decoded;
+        try{
+            decoded=jwt.verify(token,process.env.SECRET_KEY);
+        }catch(error){
+            if(error.name==="TokenExpiredError"){
+                return res.status(400).json({
+                    success:false,
+                    message:"The registeration token has expired"
+                })
+            }
+            return res.status(400).json({
+                success:"false",
+                message:"Token verification Failed"
+            })
+        }
+        const user=await User.findById(decoded.id)
+        if(!user){
+            return res.status(400).json({
+                success:false,
+                message:"User not found"
+            })
+        }
+        user.token=null;
+        user.isVerified=true;
+        await user.save();
+        return res.status(200).json({
+            success:true,
+            message:"Email verified successfully"
+        })
+    }
+    catch(error){
+        res.status(500).json({
+            success:false,
+            message:error.message,
+        })
     }
 }
