c9208fcb4de87f2a33aaf9fa2cd6324ffccd9caa
Kunal Nibrad <kunalnibrad297@gmail.com>
2026-03-04 22:20:19 +0530

    some changes stopping due to network

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,8 +1,52 @@
 import React from 'react'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/components/ui/tabs"
+
+
+
 
 const Profile = () => {
   return (
-    <div>Profile</div>
+    <div className='pt-10 min-h-screen bg-gray-100'>
+      <Tabs defaultValue="profile" className="max-w-7xl mx-auto items-center">
+        <TabsList>
+          <TabsTrigger value="profile" className='text-blue-400'>Profile</TabsTrigger>
+          <TabsTrigger value="orders">Orders</TabsTrigger>
+        </TabsList>
+        <TabsContent value="profile" className='text-blue-400'>
+          <div>
+            <div className='flex flex-col justify-center items-center bg-gray-100'>
+              <h1 className='font-bold mb-7 text-2xl '>Update Profile</h1>
+            </div>
+          </div>
+        </TabsContent>
+        <TabsContent value="orders">
+          <Card>
+            <CardHeader>
+              <CardTitle>Orders</CardTitle>
+              <CardDescription>
+                Track performance and user engagement metrics. Monitor trends and
+                identify growth opportunities.
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="text-sm text-muted-foreground">
+              Page views are up 25% compared to last month.
+            </CardContent>
+          </Card>
+        </TabsContent>
+      </Tabs>
+    </div>
   )
 }
 
